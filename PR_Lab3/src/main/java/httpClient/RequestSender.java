95bf18cd87693ac287503ea96d890f7a83f2f2ce
Alexandru Botnaru <Alexandru.Botnaru@endava.com>
2022-05-17 14:19:12 +0300

    small improvments

--- PR_Lab3/src/main/java/httpClient/RequestSender.java
+++ PR_Lab3/src/main/java/httpClient/RequestSender.java
@@ -1,69 +1,76 @@
 package httpClient;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpHeaders;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.HashMap;
 
 public class RequestSender {
     private final HttpClient httpClient = HttpClient.newHttpClient();
 
     public void sendGetRequest(String url) throws IOException, InterruptedException {
-        HttpRequest httpRequest = HttpRequest.newBuilder()
+        HttpRequest request = HttpRequest.newBuilder()
                 .GET()
                 .header("accept", "application/json")
                 .uri(URI.create(url))
                 .build();
-        HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
         System.out.println("Response code: " + response.statusCode());
         System.out.println(response.body());
     }
 
     public void sendPostRequest(String url) throws IOException, InterruptedException {
-        String requestBody = "{\n" +
-                "  \"userId\": 18,\n" +
-                "  \"title\": \"test title\",\n" +
-                "  \"body\": \"test body\",\n" +
-                "}";
+        var values = new HashMap<String, String>();
+        values.put("userId", "18");
+        values.put ("title", "test title");
+        values.put("body", "test body");
 
-        HttpRequest httpRequest = HttpRequest.newBuilder()
+        var objectMapper = new ObjectMapper();
+        String requestBody = objectMapper
+                .writeValueAsString(values);
+
+        HttpRequest request = HttpRequest.newBuilder()
                 .POST(HttpRequest.BodyPublishers.ofString(requestBody))
                 .header("accept", "application/json")
                 .uri(URI.create(url))
                 .build();
-        HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
         System.out.println("Response code: " + response.statusCode());
         System.out.println(response.body());
-        System.out.println(response);
     }
 
     public void sendHeadRequest(String url) throws IOException, InterruptedException {
-        HttpRequest headRequest = HttpRequest.newBuilder()
+        HttpRequest request = HttpRequest.newBuilder()
+                .header("accept", "application/json")
                 .method("HEAD", HttpRequest.BodyPublishers.noBody())
                 .uri(URI.create(url))
                 .build();
 
-        HttpResponse<Void> headResponse = httpClient.send(headRequest, HttpResponse.BodyHandlers.discarding());
+        HttpResponse<Void> response = httpClient.send(request, HttpResponse.BodyHandlers.discarding());
 
         System.out.println("Headers: ");
-        HttpHeaders headers = headResponse.headers();
-        headers.map().forEach((key, value) -> System.out.printf("%s = %s%n", key, value));
+        HttpHeaders headers = response.headers();
+        headers.map().forEach((key, value) -> System.out.printf("%s = %s\n", key, value));
     }
 
     public void sendOptionsRequest(String url) throws IOException, InterruptedException {
-        HttpRequest optionsRequest = HttpRequest.newBuilder()
+        HttpRequest request = HttpRequest.newBuilder()
+                .header("accept", "application/json")
                 .method("OPTIONS", HttpRequest.BodyPublishers.noBody())
                 .uri(URI.create(url))
                 .build();
 
-        HttpResponse<Void> optionsResponse = httpClient.send(optionsRequest, HttpResponse.BodyHandlers.discarding());
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
-        HttpHeaders optionHeaders = optionsResponse.headers();
-        optionHeaders.map().forEach((key, value) -> System.out.printf("%s = %s%n", key, value));
+        HttpHeaders optionHeaders = response.headers();
+        optionHeaders.map().forEach((key, value) -> System.out.printf("%s = %s\n", key, value));
     }
 
 }
